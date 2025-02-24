ac5685ed277168a29f7c13e5239b04d3d11423ba
Casey <82072694+soulcasey@users.noreply.github.com>
2025-02-24 12:08:16 -0500

    Show outline

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Target : MonoBehaviour, IDamageable
@@ -6,17 +7,28 @@ public class Target : MonoBehaviour, IDamageable
     private float currentHealth;
 
     private HealthBar healthBar;
+    private Outline outline;
+    private Coroutine outlineCoroutine;
 
     private const float HEALTHBAR_Y_OFFSET = 0.8f;
 
     private void Start()
     {
         currentHealth = maxHealth;
-        
+
         Vector3 hbPosition = new Vector3(transform.position.x, transform.position.y + HEALTHBAR_Y_OFFSET, transform.position.z);
         HealthBar hb = Instantiate(Resources.Load<HealthBar>($"Prefabs/HealthBar"), hbPosition, Quaternion.identity, transform);
         healthBar = hb.GetComponent<HealthBar>();
         healthBar.gameObject.SetActive(false);
+
+        if (outline == null)
+        {
+            outline = gameObject.AddComponent<Outline>();
+        }
+        
+        outline.enabled = false;
+        outline.OutlineWidth = 10;
+        outline.OutlineColor = Color.red;
     }
 
     public void OnHit(float damage)
@@ -25,9 +37,28 @@ public class Target : MonoBehaviour, IDamageable
         healthBar.gameObject.SetActive(true);
         healthBar.UpdateBar(currentHealth / maxHealth);
 
+        if (outlineCoroutine != null)
+        {
+            StopCoroutine(outlineCoroutine);
+        }
+        outlineCoroutine = StartCoroutine(ShowOutline(0.5f));
+
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator ShowOutline(float duration)
+    {
+        outline.enabled = true;
+        yield return new WaitForSeconds(duration);
+        outline.enabled = false;
+    }
+
+    // Test
+    private void OnMouseDown()
+    {
+        OnHit(10);
     }
 }
