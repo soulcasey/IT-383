b8e178974a2652787af4ab868a59c9b366a91050
Casey <82072694+soulcasey@users.noreply.github.com>
2025-02-24 11:57:33 -0500

    Add outline, adjust target and healthbar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,24 +5,13 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Image bar;
-    public RotationConstraint rotationConstraint;
-
-    private void Start()
-    {
-        // rotationConstraint.AddSource(new ConstraintSource()
-        // {
-        //     sourceTransform = Camera.main.transform,
-        //     weight = 1
-        // });
-        // rotationConstraint.constraintActive = true;
-    }
 
     public void UpdateBar(float ratio)
     {
         bar.fillAmount = ratio;
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         transform.LookAt(Camera.main.transform); // Always face the camera
     }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,26 +5,25 @@ public class Target : MonoBehaviour, IDamageable
     public float maxHealth = 100f;
     private float currentHealth;
 
-    public GameObject healthBarPrefab;
-    private HealthBar healthBarInstance;
+    private HealthBar healthBar;
 
     private const float HEALTHBAR_Y_OFFSET = 0.8f;
 
-    void Start()
+    private void Start()
     {
         currentHealth = maxHealth;
-
+        
         Vector3 hbPosition = new Vector3(transform.position.x, transform.position.y + HEALTHBAR_Y_OFFSET, transform.position.z);
-        GameObject hb = Instantiate(healthBarPrefab, hbPosition, Quaternion.identity, transform);
-        healthBarInstance = hb.GetComponent<HealthBar>();
-        hb.SetActive(false); // Hide initially
+        HealthBar hb = Instantiate(Resources.Load<HealthBar>($"Prefabs/HealthBar"), hbPosition, Quaternion.identity, transform);
+        healthBar = hb.GetComponent<HealthBar>();
+        healthBar.gameObject.SetActive(false);
     }
 
     public void OnHit(float damage)
     {
         currentHealth -= damage;
-        healthBarInstance.gameObject.SetActive(true); // Show health bar when hit
-        healthBarInstance.UpdateBar(currentHealth / maxHealth);
+        healthBar.gameObject.SetActive(true);
+        healthBar.UpdateBar(currentHealth / maxHealth);
 
         if (currentHealth <= 0)
         {
